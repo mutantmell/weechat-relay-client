e3aeba7510c7700eb76163330b1ad78038ca963b
Matt Mullen <malaguy@gmail.com>
2021-03-20 15:54:54 -0700

    add code for an event listener

--- src/client.ts
+++ src/client.ts
@@ -1,3 +1,4 @@
+import * as Message from './protocol/message';
 import * as Request from './protocol/request';
 
 export interface Config {
@@ -7,20 +8,30 @@ export interface Config {
 
 export class Weechat {
     private ws: WebSocket;
+    private parser: Message.MessageParser;
 
-    private constructor (ws: WebSocket) {
+    private constructor (ws: WebSocket, parser: Message.MessageParser) {
         this.ws = ws;
+        this.parser = parser;
     }
 
     public static make(config: Config): Weechat {
         const ws = new WebSocket(
             `ws://${config.url}:${config.port}`
         );
-        return new Weechat(ws);
+        const parser = new Message.MessageParser(
+            new TextDecoder()
+        );
+        return new Weechat(ws, parser);
     }
 
     public send(requests: [Request.Request, ...Request.Request[]]): void {
         this.ws.send(requests.map((req) => Request.format(req)).join());
+    }
+
+    public register(onMessage: (msg: Message.Message) => void) {
+        // this is inefficient, as we have to parse once/consumer, but eh
+        this.ws.addEventListener('message', event => onMessage(this.parser.parse(event.data)))
     }
 
 }
